a91bd585733e01db86255233b6dc0ce0bfe05801
garshishka <garshishka@yandex.ru>
2023-11-03 16:39:08 +0300

    Added first pass of collapsibale toolbar

--- app/src/main/java/ru/garshishka/testdelivery/ui/main/MainFragment.kt
+++ app/src/main/java/ru/garshishka/testdelivery/ui/main/MainFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
+import ru.garshishka.testdelivery.R
 import ru.garshishka.testdelivery.databinding.FragmentMainBinding
 import ru.garshishka.testdelivery.ui.viewholder.FoodListAdapter
 import ru.garshishka.testdelivery.webapi.DataFeedState
@@ -39,6 +42,19 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    private fun bindSpinner(){
+        this.context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.location,
+                android.R.layout.simple_spinner_item
+                ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+                binding.dropdownLocation.adapter = adapter
+            }
+        }
+    }
+
     private fun subscribe(){
         binding.apply {
             foodList.adapter = adapter
@@ -46,6 +62,7 @@ class MainFragment : Fragment() {
                 viewModel.load()
             }
         }
+        bindSpinner()
 
         viewModel.apply {
             foodData.observe(viewLifecycleOwner){
